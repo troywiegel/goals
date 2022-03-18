a9a0f9202dd1758854ee48654801265ef3eddc69
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2022-03-18 11:27:57 -0700

    finished REST API for app

---
+++ server/routes/goalRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+
+router.route("/").get(getGoals).post(setGoal);
+
+router.route("/:id").put(updateGoal).delete(deleteGoal);
+
+module.exports = router;
