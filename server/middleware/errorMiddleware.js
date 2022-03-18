0f03a7583dbf2e9f7c3718cd126a02ba97e37dce
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2022-03-18 15:37:14 -0700

    added user authentication and authorization

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,9 +1,9 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
 
-  res.staus(statusCode);
+  res.status(statusCode);
 
-  restart.json({
+  res.json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
